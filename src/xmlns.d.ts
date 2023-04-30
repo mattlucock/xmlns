5553ce48f724ecc1ce0b8cc25a411dabdc81f63a
Matt Lucock <2455682+mattlucock@users.noreply.github.com>
2023-04-30 13:29:42 +1000

    Initial commit

---
+++ src/xmlns.d.ts
@@ -0,0 +1,4 @@
+export const MATHML_NS: 'http://www.w3.org/1998/Math/MathML'
+export const SVG_NS: 'http://www.w3.org/2000/svg'
+export const TTML_NS: 'http://www.w3.org/ns/ttml'
+export const XHTML_NS: 'http://www.w3.org/1999/xhtml'
